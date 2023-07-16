066b4ee916a87de72afeff96e8647fb87a7ead27
Narender <Narender.arora464@gmail.com>
2023-07-16 22:51:18 +0530

    Initial Commit

---
+++ SDKDemo2/SDKDemo2.h
@@ -0,0 +1,18 @@
+//
+//  SDKDemo2.h
+//  SDKDemo2
+//
+//  Created by global on 16/07/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SDKDemo2.
+FOUNDATION_EXPORT double SDKDemo2VersionNumber;
+
+//! Project version string for SDKDemo2.
+FOUNDATION_EXPORT const unsigned char SDKDemo2VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SDKDemo2/PublicHeader.h>
+
+
---
+++ SDKDemoExample/SDKDemoExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SDKDemoExampleApp.swift
+//  SDKDemoExample
+//
+//  Created by global on 16/07/23.
+//
+
+import SwiftUI
+
+@main
+struct SDKDemoExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
