1af2b548fe6250d692e75d78a49f684763248b8c
Narender <Narender.arora464@gmail.com>
2023-07-16 23:02:53 +0530

    test case added

---
+++ SDKDemo2/SwiftLib.swift
@@ -0,0 +1,22 @@
+//
+//  SwiftLib.swift
+//  SDKDemo2
+//
+//  Created by global on 16/07/23.
+//
+
+import Foundation
+
+public final class SwiftLib {
+
+    let name = "SwiftLib"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
